a6cb71f045273b4cd379ef6a2fbf115e9d24fb34
GarvRajGaur <gr2708@srmist.edu.in>
2026-03-11 14:14:10 +0530

    Garv added UC-1

---
+++ BookMyStayApp/src/BookMyStayApp.java
@@ -0,0 +1,31 @@
+/**
+ * MAIN CLASS UseCase1HotelBookingApp
+ * <p>
+ * Use Case 1: Application Entry & Welcome Message
+ * <p>
+ * Description:
+ * This class represents the entry point of the
+ * Hotel Booking Management System.
+ * <p>
+ * At this stage, the application:
+ * <p>
+ * Starts execution from the main() method
+ * Displays a welcome message to the user
+ * Confirms that the system has started successfully
+ * <p>
+ * No business logic, data structures, or user input
+ * is implemented in this use case.
+ * <p>
+ * The goal is to establish a clear and predictable
+ * application startup point.
+ *
+ * @author GARV RAJ
+ * @version 1.0
+ */
+
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to BookMyStay!");
+        System.out.println("System started successfully.");
+    }
+}
